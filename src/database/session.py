d223c4304348e6434a6ba3736c1f4a23db5688fd
Artyom <delangame666@gmail.com>
2025-07-04 15:06:39 +0300

    add database

---
+++ src/database/session.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP, func
+from sqlalchemy.ext.asyncio import (
+    AsyncAttrs,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from src.core.config import settings
+
+engine = create_async_engine(url=settings.database_url)
+async_session_maker = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP, server_default=func.now(), onupdate=func.now()
+    )
