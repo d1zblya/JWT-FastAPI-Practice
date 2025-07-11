fe61b2f01c33e235d0cad841292a9c4768eb0f1e
Artyom <delangame666@gmail.com>
2025-07-11 20:13:48 +0300

    update auth router

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,16 +1,11 @@
-import uuid
 from typing import Annotated
 
 from fastapi import APIRouter, Cookie, HTTPException, status, Response, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from loguru import logger
 
-from src.auth.schemas import TokenResponse, TokenTypes
-from src.auth.service import AuthService, RefreshTokenService
-from src.core.config import settings
-from src.exceptions.auth import LogoutError
-from src.users.schemas import UserCreate, UserJWTData
-from src.users.service import UserService
+from src.auth.schemas import TokenResponse
+from src.auth.service import AuthService
+from src.users.schemas import UserCreate
 
 router = APIRouter(
     prefix="/api/auth",
@@ -20,15 +15,7 @@ router = APIRouter(
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_user(user: UserCreate):
-    try:
-        await UserService.create_user(user)
-        return {"message": "User created successfully"}
-    except Exception as e:
-        logger.error(f"Registration failed: {e}")
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Registration failed"
-        )
+    return await AuthService.register(user)
 
 
 @router.post("/login")
@@ -36,84 +23,26 @@ async def login_user(
         user: Annotated[OAuth2PasswordRequestForm, Depends()],
         response: Response
 ) -> TokenResponse:
-    user_db = await UserService.authenticate_user(email=user.username, password=user.password)  # username = email
-
-    if user_db is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    user_jwt_data = UserJWTData(
-        id=str(user_db.id),
-        role=user_db.role,
-    )
-
-    access_token = await AuthService.create_access_token(user_jwt_data)
-    refresh_token = await AuthService.create_refresh_token(user_jwt_data)
-
-    response.set_cookie(
-        key="refresh_token",
-        value=refresh_token,
-        httponly=True,
-        secure=True,
-        samesite="strict",
-        max_age=settings.auth.REFRESH_TOKEN_EXPIRE_DAYS * 24 * 60 * 60
-    )
-
-    return TokenResponse(access_token=access_token)
+    return await AuthService.login(user=user, response=response)
 
 
 @router.post("/refresh")
-async def refresh_access_token(refresh_token: str = Cookie(None)):
-    if not refresh_token:
+async def refresh_access_token(refresh_token: str = Cookie(None)) -> TokenResponse:
+    if refresh_token is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Refresh token not provided"
         )
-    try:
-        payload = await AuthService.verify_token(
-            token=refresh_token,
-            expected_type=TokenTypes.REFRESH_TOKEN_TYPE.value
-        )
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid refresh token"
-        )
 
-    user_id = payload.get("sub")
-    user_role = payload.get("role")
-
-    if not user_id:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid refresh token payload"
-        )
-
-    user_jwt_data = UserJWTData(id=user_id, role=user_role)
-    access_token = await AuthService.create_access_token(user_jwt_data)
-
-    return TokenResponse(access_token=access_token)
+    return await AuthService.refresh(refresh_token)
 
 
-@router.post("/logout")
+@router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
 async def logout_user(response: Response, refresh_token: str = Cookie(None)):
     if refresh_token:
-        try:
-            payload = await AuthService.verify_token(refresh_token, TokenTypes.REFRESH_TOKEN_TYPE.value)
-            jti = uuid.UUID(payload.get("jti"))
-            token = await RefreshTokenService.get_refresh_token_by_jti(jti)
-            if jti and token:
-                await RefreshTokenService.delete_refresh_token(jti)
-        except Exception as e:
-            raise LogoutError(f"Logout error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Refresh token not provided"
+        )
 
-    response.delete_cookie(
-        key="refresh_token",
-        httponly=True,
-        secure=True,
-        samesite="strict"
-    )
-    return {"message": "Logged out successfully"}
+    return await AuthService.logout(refresh_token=refresh_token, response=response)
