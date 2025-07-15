a0900a81abed8bb81eabaff5b5e84941b02f43d1
Artyom <delangame666@gmail.com>
2025-07-15 10:34:27 +0300

    add business router and cors

--- src/business/router.py
+++ src/business/router.py
@@ -1,7 +1,13 @@
 import uuid
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from starlette import status
 
+from src.business.schemas import CreateBusinessProfile, UpdateBusinessProfile
+from src.business.service import BusinessProfileService
+from src.users.dependencies import get_current_user
 
 router = APIRouter(
     prefix="/api/business-profile",
@@ -10,20 +16,36 @@ router = APIRouter(
 
 
 @router.get("/{business_id}")
-async def get_business(business_id: uuid.UUID):
-    pass
+async def get_business(
+        business_id: uuid.UUID,
+        user: Annotated[get_current_user, Depends()]
+):
+    return await BusinessProfileService.get_business_profile_by_id(business_id)
 
 
-@router.post("/")
-async def create_business():
-    pass
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_business(
+        business_profile: CreateBusinessProfile,
+        user: Annotated[get_current_user, Depends()]
+):
+    return await BusinessProfileService.create_business_profile(business_profile)
 
 
 @router.put("/{business_id}")
-async def update_business(business_id: uuid.UUID):
-    pass
+async def update_business(
+        business_id: uuid.UUID,
+        business_profile: UpdateBusinessProfile,
+        user: Annotated[get_current_user, Depends()]
+):
+    return await BusinessProfileService.update_business_profile(
+        business_id=business_id,
+        business_profile=business_profile
+    )
 
 
-@router.delete("/{business_id}")
-async def delete_business(business_id: uuid.UUID):
-    pass
+@router.delete("/{business_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_business(
+        business_id: uuid.UUID,
+        user: Annotated[get_current_user, Depends()]
+):
+    await BusinessProfileService.delete_business_profile(business_id)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 
-from src.users.router import router as user_router
 from src.auth.router import router as auth_router
+from src.business.router import router as business_router
+from src.core.config import settings
+from src.core.exception_handlers import add_exception_handlers
+from src.users.router import router as user_router
 
 app = FastAPI(name="JWTAuthFastAPI", version="0.0.1")
 
 app.include_router(user_router)
 app.include_router(auth_router)
+app.include_router(business_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allow_headers=["Set-Cookie", "Authorization", "Access-Control-Allow-Origin",
+                   "Access-Control-Allow-Headers", "Content-Type"]
+)
+
+add_exception_handlers(app)
 
 
 @app.get("/")
